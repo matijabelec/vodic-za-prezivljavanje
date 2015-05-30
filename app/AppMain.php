4de96bfea2b1d834223d29cfce31d23483cf0763
matijabelec <mbelec@foi.hr>
2015-05-30 21:04:06 +0200

    file AppMain is corrected now

--- app/AppMain.php
+++ app/AppMain.php
@@ -10,17 +10,14 @@ class AppMain {
     protected function __construct() {
     }
 
-    protected function __destruct() {
-
-        public function __destruct() {
-        }
-        
-        /** @return vzp\AppMain **/
-        static public function getInstance() {
-            if(is_null(self::$instance)) {
-                self::$instance = new AppMain();
-            }
-            return self::$instance;
-        }
-    }
+    protected function __destruct() {}
     
+    /** @return vzp\AppMain **/
+    static public function getInstance() {
+        if(is_null(self::$instance) ) {
+            self::$instance = new AppMain();
+        }
+        return self::$instance;
+    }
+}
+?>
