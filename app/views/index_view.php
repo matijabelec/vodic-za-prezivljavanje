e88c82ca2f7cc2f599f5be86362fb6547d5e924d
matijabelec <mbelec@foi.hr>
2015-06-11 18:20:25 +0200

    syntax cleaned

--- app/views/index_view.php
+++ app/views/index_view.php
@@ -3,7 +3,6 @@
 class Index_view extends Webpage_view {
     public function output() {
         $content = new Template('body/index');
-        $userprofile = new Template('data/user_profile_menu_login');
         
         $page = new Standard_template('Početna');
         $page->set_body($content->fill() );
@@ -13,15 +12,9 @@ class Index_view extends Webpage_view {
     }
     
     public function admin() {
-        $user = Auth::get_user();
-        
         $content = new Template('body/index-auth');
         
-        $userprofile = new Template('data/user_profile_menu');
-        $userprofile->set('username-link', $user['username']);
-        $userprofile->set('username', $user['username']);
-        
-        $page = new Standard_template('Početna -- registrirani korisnik');
+        $page = new Standard_template('Početna');
         $page->set_body($content->fill() );
         $page->set('option-home', ' selected');
         
